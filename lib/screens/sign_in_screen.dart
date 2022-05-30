7344ee5c8ef044d105b5d90cfad05d41a1926a1b
Yassin Maher <professor.yassin@yahoo.com>
2022-05-30 22:04:37 +0200

    Input Field Widget Is Extracted

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,7 +1,8 @@
+import 'package:chat_app/widgets/InputField.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
-  SignIn({Key? key}) : super(key: key);
+  const SignIn({Key? key}) : super(key: key);
 
   @override
   State<SignIn> createState() => _SignInState();
@@ -16,147 +17,113 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(23, 27, 35, 1.0),
-      body: SafeArea(
-        child: SizedBox(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                'Sign In',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 42,
-                  fontWeight: FontWeight.w600,
-                ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Sign In',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 42,
+                fontWeight: FontWeight.w600,
               ),
-              Form(
-                  key: _formKey,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Email field is required';
-                            }
-                            return null;
-                          },
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            hintText: 'Email',
-                            fillColor: const Color.fromRGBO(0, 0, 0, 0.25),
-                            filled: true,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              borderSide: const BorderSide(
-                                width: 0,
-                                style: BorderStyle.none,
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
-                        TextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Password Field is required';
-                            }
-                            return null;
-                          },
-                          obscureText: !_isPasswordVisible,
-                          decoration: InputDecoration(
-                            hintText: 'Password',
-                            fillColor: const Color.fromRGBO(0, 0, 0, 0.25),
-                            filled: true,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              borderSide: const BorderSide(
-                                style: BorderStyle.none,
-                                width: 0,
-                              ),
-                            ),
-                            suffixIcon: IconButton(
-                              icon: Icon(_isPasswordVisible
-                                  ? Icons.visibility
-                                  : Icons.visibility_off),
-                              color: Colors.white60,
-                              onPressed: () {
-                                setState(() {
-                                  _isPasswordVisible = !_isPasswordVisible;
-                                });
-                              },
-                            ),
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Checkbox(
-                                  value: _isRememberMeChecked,
-                                  side: const BorderSide(
-                                    color: Colors.white60,
-                                  ),
-                                  activeColor: Colors.white60,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _isRememberMeChecked = value!;
-                                    });
-                                  },
+            ),
+            Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      InputField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Email field is required';
+                          }
+                          return null;
+                        },
+                        hintText: 'Email',
+                        inputType: TextInputType.emailAddress,
+                      ),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      InputField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Password field is required';
+                          }
+                          return null;
+                        },
+                        hintText: 'Password',
+                        isPassword: true,
+                        inputType: TextInputType.visiblePassword,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: _isRememberMeChecked,
+                                side: const BorderSide(
+                                  color: Colors.white60,
                                 ),
-                                const Text('Remember Me'),
-                              ],
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              child: const Text(
-                                'Forgot Your Password',
-                                style: TextStyle(
-                                  decoration: TextDecoration.underline,
-                                ),
+                                activeColor: Colors.white60,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _isRememberMeChecked = value!;
+                                  });
+                                },
                               ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 5.0,
-                        ),
-                        Container(
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: Colors.black54,
-                            borderRadius: BorderRadius.circular(10.0),
+                              const Text('Remember Me'),
+                            ],
                           ),
-                          child: MaterialButton(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text('Login in progress'),
-                                  ),
-                                );
-                              }
-                            },
+                          TextButton(
+                            onPressed: () {},
                             child: const Text(
-                              'Login',
+                              'Forgot Your Password',
                               style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0,
+                                decoration: TextDecoration.underline,
                               ),
                             ),
                           ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 5.0,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.black54,
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
-                      ],
-                    ),
-                  )),
-            ],
-          ),
+                        child: MaterialButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Login in progress'),
+                                ),
+                              );
+                            }
+                          },
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/InputField.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatefulWidget {
+  final Function validator;
+  final String hintText;
+  final bool isPassword;
+  final TextInputType inputType;
+
+  const InputField({
+    Key? key,
+    required this.validator,
+    required this.hintText,
+    required this.inputType,
+    this.isPassword = false,
+  }) : super(key: key);
+
+  @override
+  State<InputField> createState() => _InputFieldState();
+}
+
+class _InputFieldState extends State<InputField> {
+  bool isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: (value) => widget.validator(value),
+      keyboardType: widget.inputType,
+      obscureText: widget.isPassword && !isPasswordVisible,
+      decoration: InputDecoration(
+        hintText: widget.hintText,
+        fillColor: const Color.fromRGBO(0, 0, 0, 0.25),
+        filled: true,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: const BorderSide(
+            width: 0,
+            style: BorderStyle.none,
+          ),
+        ),
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                icon: Icon(isPasswordVisible
+                    ? Icons.visibility
+                    : Icons.visibility_off),
+                color: Colors.white60,
+                onPressed: () {
+                  setState(() {
+                    isPasswordVisible = !isPasswordVisible;
+                  });
+                },
+              )
+            : null,
+      ),
+    );
+  }
+}
